535fdd9d982196692cbed659450d7d41479bed63
NPapp7 <npapp86@gmail.com>
2019-06-10 15:17:46 +0200

    The task of projecting data onto principal components completed

--- machine-learning-ex7/ex7/projectData.m
+++ machine-learning-ex7/ex7/projectData.m
@@ -17,10 +17,13 @@ Z = zeros(size(X, 1), K);
 %                    x = X(i, :)';
 %                    projection_k = x' * U(:, k);
 %
-
-
-
-
+n = size(X,1)
+for i=1 : n,
+    for j=1 : K,
+        x = X(i, :)'; #projection_k renamed to kProjection
+        kProjection = x' * U(:, j);
+        Z(i, j) = kProjection;
+    end
+end
 % =============================================================
-
 end
